19a729083642d50edb3aae91f7bc35937056e6c2
Edwin Kayang <epkayang@gmail.com>
2024-03-15 15:24:48 -0700

    format changes

--- hpc.cpp
+++ hpc.cpp
@@ -31,32 +31,10 @@ namespace{
                 // Value *NewInst = builder.CreateMul(builder.getInt32(31), builder.getInt32(53));
                 // builder.CreateRet(ConstantInt::get(Type::getInt32Ty(F.getContext()), 42));
 
-
-
                 FunctionType *printfFuncType = FunctionType::get(builder.getInt32Ty(), {builder.getPtrTy()}, true);
                 Function *printfFunc = Function::Create(printfFuncType, Function::ExternalLinkage, "printf", F.getParent());
 
                 // ASSEMBLY
-
-                // Create a vector of operands
-                // Create the inline assembly instruction
-                // InlineAsm *loadHPCVal = InlineAsm::get(
-                //     FunctionType::get(Type::getVoidTy(F.getContext()), false),
-                //     "li a5, 420",
-                //     "",
-                //     true // Can throw
-                // );
-                // builder.CreateCall(loadHPCVal, {});
-
-                // InlineAsm *startHPC = InlineAsm::get(
-                //     FunctionType::get(Type::getVoidTy(F.getContext()), false),
-                //     "csrr a5, time",
-                //     "",
-                //     true // Can throw
-                // );
-                // builder.CreateCall(startHPC, {});
-
-                // Allocate memory for the value read from CSR
                 AllocaInst *csrValuePtr = builder.CreateAlloca(builder.getInt64Ty(), nullptr, "csr_value");
 
                 // Inline assembly to read value from CSR directly into the variable
@@ -70,42 +48,16 @@ namespace{
 
                 builder.CreateStore(csrValueBefore, csrValuePtr);
 
-                    // Load the value read from CSR
-                // Value *val = builder.CreateLoad(builder.getInt64Ty(), csrValueBefore);
-
                 // Call printf
                 Value *str = builder.CreateGlobalStringPtr("Before :: Value read from CSR: %lld\n");
                 builder.CreateCall(printfFunc, {str, csrValueBefore});
 
 
-                // Create the inline assembly
-                // FunctionCallee InlineAsmPrototype = F.getParent()->getOrInsertFunction(
-                //     "llvm.riscv.csrr",
-                //     Type::getInt32Ty(F.getContext()),
-                //     Type::getInt32Ty(F.getContext())
-                // );
-
-                // // Create the inline assembly function call
-                // Value *AsmCall = builder.CreateCall(
-                //     InlineAsmPrototype,
-                //     builder.getInt32(0x300), // mstatus CSR number
-                //     "csrr"
-                // );
-
-                // // Store the result in register a0
-                // Value* reg = builder.CreateAlloca(Type::getInt32Ty(F.getContext()), nullptr, "a0");
-                // builder.CreateStore(AsmCall, reg);
-                // ASSEMBLY
-
-                // Iterate over instructions in the last basic block
                 Instruction* returnInst = LastBB->getTerminator();
                 builder.SetInsertPoint(returnInst);
 
 
                 Value* csrValueAfter = builder.CreateCall(readCSR, {});
-
-                // Load the value read from CSR
-                // val = builder.CreateLoad(builder.getInt64Ty(), csrValueAfter);
 
                 // Call printf
                 Value *str2 = builder.CreateGlobalStringPtr("After :: Value read from CSR: %lld\n");
